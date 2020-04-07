0fc7156e04fe619d35611307f1f46c09e67ebf00
André Chibeles <achibel@cilnet.pt>
2020-04-07 17:04:10 +0100

    Simple CRUD working

--- back-end/todoapp/models/item.py
+++ back-end/todoapp/models/item.py
@@ -5,10 +5,23 @@ def sql_query(query, *args):
     cur = conn.cursor()
     cur.execute(query, args)
     result = cur.fetchone()
-    print(result)
     cur.close()
     conn.close()
     return result
+
+def sql_query_insert(query, args):
+    try:
+        conn = psycopg2.connect("dbname=Todo-app user=postgres password=123")
+        cur = conn.cursor()
+        cur.execute(query, args)
+        conn.commit()
+        cur.close()
+        conn.close()
+        return True
+    except Exception as e:
+        print(e)
+        return False
+
 
 class Item():
 
@@ -33,6 +46,30 @@ class Item():
         result = sql_query(query, (id,))
         print(result)
         return cls(*result)
+    
+    def create_item(item_name):
+        query = "INSERT INTO items (item_name, item_state) VALUES (%s, %s)"
+        if sql_query_insert(query, (item_name, False)):
+            print("Item inserted with success")
+            return 
+        print("A problem in insertion")
+    
+    def update_item(self, id, data):
+        query = "UPDATE items SET item_name=%s, item_state=%s WHERE item_id=%s"
+        print("update_item")
+        print(data)
+        if sql_query_insert(query, (data[0], data[1], id)):
+            print("Item Updated with success")
+            return
+        print("Problem in update")
+
+    def delete_item(item_id):
+        query = "DELETE FROM items WHERE item_id=%s"
+        if sql_query_insert(query, (item_id,)):
+            print("Item Deleted with success")
+            return
+        print("Problem deleting item")
+
 
 
 
--- back-end/todoapp/test_item.py
+++ back-end/todoapp/test_item.py
@@ -4,4 +4,13 @@ import psycopg2
 
 item = Item.find_by_id(1)
 print(item)
+#Item.create_item("Teste 3")
+#item2 = Item.find_by_id(8)
+
+item_update = Item.find_by_id(4)
+data = ["Update Teste", True]
+item_update.update_item(4, data)
+#Item.delete_item(10)
+
+
 
